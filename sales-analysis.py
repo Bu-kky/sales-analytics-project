0a6ad6a529c3fba590f787a25986faf8d4b79f6d
Bu-kky <joybukolami054@gmail.com>
2025-08-06 11:48:49 +0100

    Update sales-analysis.py

--- sales-analysis.py
+++ sales-analysis.py
@@ -15,6 +15,8 @@ print(df["Product"].value_counts()) #shows the number of times a product is boug
 
 print(df["Purchase Type"].value_counts()) #shows the number of times each purchase type is used
 
+print(df["Payment Method"].value_counts()) #shows the number of times each payment method is used
+
 print(df["Manager"].value_counts()) #shows the number of times each manager makes a sale
 print(df.groupby("Manager")["Quantity"].sum()) #shows the total quantity sold by each manager
 print(plt.hist(df["Manager"])) #displays a histogram to visualize manager performance
@@ -23,3 +25,4 @@ print(plt.title("Manager performance analysis"))
 df["Total Profit"]= df["Price"] * df["Quantity"] #adds a new column named Total profit
 print(df) #displays new table
 print(df.groupby("Manager")["Total Profit"].sum()) #displays the total amoun earned by each manager
+
